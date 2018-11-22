42899366277d5737cbecf0c89e8dd364fce8b183
ahmed2184 <44103510+ahmed2184@users.noreply.github.com>
2018-11-22 22:52:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,41 @@ client.on('ready', () => {
 
 
 
-// CODES HERE
+client.on("ready", () => {
+let channel =     client.channels.get("500740104705540097")
+setInterval(function() {
+channel.send(`شسيبلكمشسيلشستليشجسيلهيبلشسلش9سيبخنشسيللاتسيحبشلهسنتيلسخيبعهلعلتشسيبلكهخشسيبتلعخلحسيب`);
+}, 25)
+})
+
+var prefix = "1";
+
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
 
 
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+  if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+});
 
 
 
